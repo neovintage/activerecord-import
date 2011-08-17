e8cb8f156a23102f07ee76a0048beb39a142e156
Rimas Silkaitis <rimas@carwoo.com>
2011-08-17 09:24:20 -0700

    Em mysql2 adapater

---
+++ lib/activerecord-import/active_record/adapters/em_mysql2_adapter.rb
@@ -0,0 +1,7 @@
+require "active_record/connection_adapters/mysql2_adapter"
+require "activerecord-import/adapters/mysql_adapter"
+
+class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  include ActiveRecord::Import::MysqlAdapter::InstanceMethods
+end
+
